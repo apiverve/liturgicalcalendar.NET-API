2a5ba43121c26bf8bc55a6cce5e0f2164943942a
APIVerve Team <hello@apiverve.com>
2025-02-21 07:44:30 -0600

    Added new API Endpoints

---
+++ LiturgicalCalendarQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class LiturgicalCalendarQueryOptions {
+public integer month { get; set; }
+public integer year { get; set; }
+}
+
+}
